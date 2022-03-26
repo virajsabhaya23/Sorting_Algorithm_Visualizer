928858710d98abe35e2f526ef3dfa43e180b48f9
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-26 03:58:29 -0500

    Visualizer Draw info created

--- SortingAlgoVisualizer.py
+++ SortingAlgoVisualizer.py
@@ -1,4 +1,19 @@
-import pygame
 import random
+import pygame
 pygame.init()
 
+class DrawInfo:
+    BLACK = 0,0,0
+    WHITE = 255,255,255
+    RED =  255,0,0
+    GREEN = 0,255,0
+    BLUE = 0,0,255
+    GREY = 128,128,128
+    BG = 24,24,24
+    BACKGROUND_COLOR = BG
+
+    def __init__(self,width,height,lst):
+        self.width = width
+        self.height = height
+        
+        self.widnow = pygame.display.set_mode((width, height))
